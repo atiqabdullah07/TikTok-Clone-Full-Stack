c40e98772a25aea76cde88a6ddfb895b3482ae5f
AtiqAbdullah <atiqabdullah86@gmail.com>
2023-09-23 17:04:05 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:tik_tok_clone/Views/Auth/login_screen.dart';
 import 'package:tik_tok_clone/Views/home_screen.dart';
-import 'package:tik_tok_clone/Views/testing.dart';
 
 import 'Constants/constants.dart';
 import 'Controller Bindings/controller_bindings.dart';
@@ -19,6 +19,6 @@ void main() async {
             title: 'Tik Tok',
             theme: ThemeData.dark()
                 .copyWith(scaffoldBackgroundColor: backgroundColor),
-            home: HomeScreen(),
+            home: LoginScreen(),
           )));
 }
